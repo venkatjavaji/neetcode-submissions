cd1e5037d830f0f41d26786d0ed085909432d591
venkat javaji <28551918+venkatjavaji@users.noreply.github.com>
2026-07-13 20:35:48 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Map<Character,Character> pMap = new HashMap<>();
+        pMap.put(')','(');
+        pMap.put(']', '[');
+        pMap.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(pMap.containsValue(c)) {
+                stack.push(c);
+            } else if(!stack.isEmpty() && stack.peek()==pMap.get(c)){
+                    stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    
+    }
+}
