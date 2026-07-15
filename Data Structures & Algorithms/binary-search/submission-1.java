33b511bf6268834e5118340e577cc78840171e70
venkat javaji <28551918+venkatjavaji@users.noreply.github.com>
2026-07-14 23:01:54 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int len = nums.length;
+        int left = 0;
+        int right = len-1;
+
+        int result = -1;
+        while(left<=right) {
+            int mid = (left + right)/2;
+
+            // System.out.println("mid: "+mid);
+
+            if(nums[mid] == target) {
+                result = mid;
+                break;
+            } else if(nums[mid] < target) {
+                    left = mid+1;
+                    // System.out.println("left: "+left);
+            } else {
+                right = mid-1;
+                //  System.out.println("right: "+right);
+            }
+        }
+        return result;
+        
+    }
+}
