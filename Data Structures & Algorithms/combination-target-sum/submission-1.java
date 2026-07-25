f41843f42c2e843bffa504add64307e91c8f3720
venkat javaji <28551918+venkatjavaji@users.noreply.github.com>
2026-07-25 09:33:13 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        dfsum(result, new ArrayList<>(), nums, target, target, 0);
+        return result;
+    }
+
+    public void dfsum(List<List<Integer>> result, List<Integer> temp, int[] nums,
+        int target, int remaining, int counter) {
+
+            if(remaining == 0) {
+                result.add(new ArrayList<>(temp));
+                return;
+            }
+            for(int i = counter; i<nums.length; i++) {
+                if(nums[i]>remaining) break; //if the current element in given array reaches ramaining
+                temp.add(nums[i]);
+                dfsum(result, temp, nums, target, remaining-nums[i], i); //sending the same i, to consider the current element indefinately...
+                temp.remove(temp.size()-1);
+            }
+    }
+}
